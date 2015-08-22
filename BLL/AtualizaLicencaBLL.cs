6b034cb28f988ed28e10d1bfb8b167a85f8f874a
Gustavo Gonçalez <guuhs@me.com>
2015-08-22 00:05:13 -0300

    0000ghg

---
+++ BLL/AtualizaLicencaBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+using NORD.Modelos;
+using NORD.DAL;
+
+namespace NORD.BLL
+{
+    public class AtualizaLicencaBLL
+    {
+        public void validar(AtualizaLicenca atualiza)
+        {
+            if(atualiza.Codigo.Trim().Length == 0)
+            {
+                throw new Exception("O campo SERIAL não pode estar vazio !");
+            }
+        }
+    }
+}
